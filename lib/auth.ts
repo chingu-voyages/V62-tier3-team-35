9025fd58963c240afceb710fc1c68dfb956c22bf
fabirez <skyc4ff3ine@virgilio.it>
2026-09-20 00:56:27 +0200

    feat(auth): configure better-auth server, client SDK and zod schemas

---
+++ lib/auth.ts
@@ -0,0 +1,52 @@
+import { betterAuth } from "better-auth";
+
+export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  emailAndPassword: {
+    enabled: true,
+    async sendResetPassword({ user, url }) {
+      console.log(`Reset password link for ${user.email}, ${url}`)
+      // await sendEmail({
+      // 	to: user.email,
+      // 	subject: "Reset your password",
+      // 	template: "reset-password",
+      // 	variables: {
+      // 		userEmail: user.email,
+      // 		resetLink: url,
+      // 		userName: user.name,
+      // 	},
+      // });
+    },
+  },
+  emailVerification: {
+    sendOnSignUp: true,
+    autoSignInAfterVerification: true,
+    async sendVerificationEmail({ user, url }) {
+      console.log(`Verification link for ${user.email}, ${url}`)
+      // await sendEmail({
+      //   to: user.email,
+      //   subject: "Verify your email address",
+      //   template: "verify-email",
+      //   variables: {
+      //     verificationUrl: url,
+      //     userEmail: user.email,
+      //     userName: user.name,
+      //     appName: "Pathway",
+      //     expirationMinutes: "10",
+      //     verificationCode: "",
+      //   },
+      // });
+    },
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+  },
+});
--- lib/schemas/auth.ts
+++ lib/schemas/auth.ts
@@ -1,21 +1,25 @@
 import * as z from "zod";
 
+
+const passwordSchema = z
+  .string()
+  .min(1, "Password required")
+  .min(8, "Minimum 8 characters")
+  .max(128, "Maximum 128 characters")
+  .regex(/[a-z]/, "Must contain at least one lowercase letter")
+  .regex(/[A-Z]/, "Must contain at least one uppercase letter")
+  .regex(/[0-9]/, "Must contain at least one number")
+  .regex(/[^A-Za-z0-9]/, "Must contain at least one special character")
+  .trim();
+
+
 export const signUpSchema = z.object({
   fullName: z.string().trim().min(1, "Full name required"),
   email: z
     .email("Please provide a right email")
     .trim()
     .min(1, "Email required"),
-  password: z
-    .string()
-    .min(1, "Password required")
-    .min(8, "Minimum 8 characters")
-    .max(256, "Maximum characters 128")
-    .regex(/[a-z]/, "Must contain at least one lowercase letter")
-    .regex(/[A-Z]/, "Must contain at least one uppercase letter")
-    .regex(/[0-9]/, "Must contain at least one number")
-    .regex(/[^A-Za-z0-9]/, "Must contain at least one special character")
-    .trim(),
+  password: passwordSchema
 });
 
 export const loginSchema = z.object({
@@ -26,5 +30,24 @@ export const loginSchema = z.object({
   password: z.string().min(1, "Password required").trim(),
 });
 
+export const forgotPasswordSchema = z.object({
+  email: z
+    .email("Please provide a valid email")
+    .trim()
+    .min(1, "Email is required"),
+});
+
+export const resetPasswordSchema = z
+  .object({
+    password: passwordSchema,
+    confirmPassword: z.string().min(1, "Please confirm your password").trim(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
 export type signUpType = z.infer<typeof signUpSchema>;
 export type loginType = z.infer<typeof loginSchema>;
+export type forgotPasswordType = z.infer<typeof forgotPasswordSchema>;
+export type resetPasswordType = z.infer<typeof resetPasswordSchema>;
