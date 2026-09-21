081532eabf6942bfedcf4d85768ea3694233fb90
fabirez <skyc4ff3ine@virgilio.it>
2026-09-21 15:24:49 +0200

    fix(layout): add --navbar-height css variable and fix auth layout overflow

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-dvh w-screen bg-background flex justify-center items-center">
+    <div className="h-[calc(100dvh-var(--navbar-height))] w-full bg-background flex justify-center items-center">
       <div className="grid md:grid-cols-2 max-w-(--breakpoint-xl) w-full p-4 sm:px-10">
         <div className="bg-card rounded-2xl md:rounded-l-2xl py-5 px-4 sm:py-10  md:px-8 lg:px-10 xl:px-16 shadow-2xl flex flex-col w-full">
           <Logo size="lg" variant="text" className="mb-14 sm:mb-20" />
